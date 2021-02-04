595b7132b4e22b02cf7b46d0c5f411b75ed29a31
tecunMartin <tecunmartin132@gmail.com>
2021-02-04 02:47:44 -0600

    Terminamos la pagina 404

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,5 +1,23 @@
+/* eslint-disable react/jsx-no-target-blank */
 import React from 'react';
+import './styles/NotFound.scss';
+const NotFound = () => {
+  return (
+    <section className="todo">
+      <section class="er-complete">
+        <span>4</span>
+        <span>
+          <span class="zero-text">0</span>
+        </span>
+        <span>4</span>
+      </section>
+      <div class="link-container">
+        <a href="/" className="linkss">
+          Regresar al Home para ver tu producto
+        </a>
+      </div>
+    </section>
+  );
+};
 
-export default function NotFound() {
-  return <div>Error404</div>;
-}
+export default NotFound;
